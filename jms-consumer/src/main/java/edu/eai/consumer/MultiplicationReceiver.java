436f3a931b2e07622a10a159215c1a32d4a69eef
patrick <patricklindner@outlook.com>
2022-11-22 10:59:09 +0100

    added thread sleep in multiplication to simulate heavy processing for queue example

--- jms-consumer/src/main/java/edu/eai/consumer/MultiplicationReceiver.java
+++ jms-consumer/src/main/java/edu/eai/consumer/MultiplicationReceiver.java
@@ -3,6 +3,7 @@ package edu.eai.consumer;
 import edu.eai.domain.NumberMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -12,6 +13,10 @@ import org.springframework.stereotype.Component;
 @ConditionalOnProperty(name = "mode", havingValue = "multiplication")
 public class MultiplicationReceiver {
 
+    @Value("${channel}")
+    private String channel;
+
+
     public MultiplicationReceiver() {
         LOGGER.info("Multiplication Receiver started.");
     }
@@ -20,8 +25,12 @@ public class MultiplicationReceiver {
 
     @JmsListener(destination = "numbers")
     @SendTo("results")
-    public String receive(NumberMessage message) {
+    public String receive(NumberMessage message) throws InterruptedException {
         LOGGER.info("Multiplying numbers: {} * {}", message.getNumber1(), message.getNumber2());
+        if(channel.equals("queue")) {
+            LOGGER.info("Waiting for multiplication...");
+            Thread.sleep(5000);
+        }
         int result = message.getNumber1() * message.getNumber2();
         LOGGER.info("Result: {}", result);
         return "Result of multiplication: " + result;
